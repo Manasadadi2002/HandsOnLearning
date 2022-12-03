bb395ada45182ab47e0dc545d88a1193ead253d8
Manasadadi2002 <115692768+Manasadadi2002@users.noreply.github.com>
2022-12-03 13:19:14 +0000

    HOL2

---
+++ HOL2/prodcutsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOL1.Controllers
+{
+    public class prodcutsController : Controller
+    {
+        // GET: prodcuts
+        public ActionResult Index()
+        {
+            Models.product p = new Models.product();
+            return View(p.GetProducts());
+        }
+    }
+}
